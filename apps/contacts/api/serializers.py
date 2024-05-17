adff838f912533258af1cd56d6dd901cd8a2c701
Miguel Angel López <miguel.lopezm.dev@gmail.com>
2024-05-16 23:50:40 -0600

    Added readme.md

--- apps/contacts/api/serializers.py
+++ apps/contacts/api/serializers.py
@@ -141,7 +141,6 @@ class ContactsRegisterSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, data):
-        print(f"Esta es la Data: {data}")
         if data["name"] == "" or data["last_name"] == "" or data["phones"] == "":
             raise serializers.ValidationError(
                 "Debe registrar su nombre y apellido y por lo menos un telefono"
@@ -150,14 +149,15 @@ class ContactsRegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """
-        Create a new contact instance and associated phone numbers.
+        Create a new contact instance and associated related objects.
 
         This method takes the validated data from the serializer, creates a new contact
-        instance in the database, and then iterates over the phone numbers provided in the
-        validated data to create corresponding phone records in the database.
+        instance in the database, and then iterates over the related objects provided in the
+        validated data to create corresponding records in the database.
 
         Parameters:
-        None
+        validated_data (dict): A dictionary containing the validated data from the serializer.
+            This data includes the contact details, as well as lists of related objects (phones, emails, etc.).
 
         Returns:
         contact (Contacts): The newly created contact instance.
@@ -166,126 +166,56 @@ class ContactsRegisterSerializer(serializers.ModelSerializer):
         None
 
         """
-        # print(f"Validated_data: {self.validated_data} ")
-        # contact = super().save(**kwargs)
-        # # contact = Contacts.objects.create(
-        # #     name=self.validated_data["name"],
-        # #     last_name=self.validated_data["last_name"],
-        # #     user=self.validated_data["user"],
-        # # )
-
-        # # Associates the phones to created contact
-        # phones_data = self.validated_data.get("phones", [])
-        # for phone_data in phones_data:
-        #     phone_number = phone_data["phone"]["phone"]
-        #     phone_type = phone_data["phone_type"]
-        #     Phones.objects.create(
-        #         contact=contact, phone=phone_number, phone_type=phone_type
-        #     )
-
-        # # Associates the emails to created contact
-        # emails_data = self.validated_data.get("emails", [])
-        # for email_data in emails_data:
-        #     email = email_data["email"]
-        #     email_type = email_data["email_type"]
-        #     Emails.objects.create(contact=contact, email=email, email_type=email_type)
-
-        # # Associates the address to created contact
-        # addresses_data = self.validated_data.get("address", [])
-        # for address_data in addresses_data:
-        #     address = address_data["address"]
-        #     address_type = address_data["address_type"]
-        #     Address.objects.create(
-        #         contact=contact, address=address, address_type=address_type
-        #     )
-
-        # # Associates the important dates to created contact
-        # important_dates_data = self.validated_data.get("important_dates", [])
-        # for important_date_data in important_dates_data:
-        #     important_date = important_date_data["important_date"]
-        #     important_date_type = important_date_data["important_date_type"]
-        #     ImportantDates.objects.create(
-        #         contact=contact,
-        #         important_date=important_date,
-        #         important_date_type=important_date_type,
-        #     )
-
-        # # Associates the related persons to created contact
-        # related_persons_data = self.validated_data.get("related_persons", [])
-        # for related_person_data in related_persons_data:
-        #     name = related_person_data["name"]
-        #     related_person_type = related_person_data["related_person_type"]
-        #     RelatedPersons.objects.create(
-        #         contact=contact,
-        #         name=name,
-        #         related_person_type=related_person_type,
-        #     )
-
-        # # Associates the tags to created contact
-        # tags_data = self.validated_data.get("tags", [])
-        # for tag_data in tags_data:
-        #     tag = tag_data["tag"]
-        #     Tags.objects.create(contact=contact, tag=tag)
-
-        # return contact
-        # Manejar la creación de objetos relacionados (nested objects) aquí
-        phones_data = validated_data.pop("phones", [])  # Obtener datos de los teléfonos
-        emails_data = validated_data.pop("emails", [])  # Obtener datos de los emails
-        addresses_data = validated_data.pop(
-            "address", []
-        )  # Obtener datos de los emails
+        phones_data = validated_data.pop("phones", [])  # Get data from phones
+        emails_data = validated_data.pop("emails", [])  # Get data from emails
+        # Get data from addresses
+        addresses_data = validated_data.pop("address", [])
+        # Get data from important dates
         important_dates_data = validated_data.pop(
             "important_dates", []
-        )  # Obtener datos de los emails
+        )  # Get data from related persons
         related_persons_data = validated_data.pop(
             "related_persons", []
-        )  # Obtener datos de los emails
-        tags_data = validated_data.pop("tags", [])  # Obtener datos de los emails
+        )  # Get data from tags
+        tags_data = validated_data.pop("tags", [])
 
-        # Crea el objeto principal (en este caso, el contacto)
+        # Create the main object (in this case, the contact)
         contact = Contacts.objects.create(**validated_data)
 
-        # Crea los objetos relacionados (en este caso, los teléfonos y emails)
         for phone_data in phones_data:
-            # Crea cada objeto de teléfono relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each phone object related to the contact
             phone = Phones.objects.create(**phone_data)
-            # Agregar el teléfono al contacto
+            # Add phone to contact
             contact.phones.add(phone)
 
         for email_data in emails_data:
-            # Crea cada objeto de email relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each email object related to the contact
             email = Emails.objects.create(**email_data)
-            # Agregar el teléfono al contacto
+            # Add email to contact
             contact.emails.add(email)
 
         for address_data in addresses_data:
-            # Crea cada objeto de email relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each address object related to the contact
             address = Address.objects.create(**address_data)
-            # Agregar el teléfono al contacto
+            # Add address to contact
             contact.address.add(address)
 
         for important_date_data in important_dates_data:
-            # Crea cada objeto de email relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each iportante date object related to the contact
             important_date = ImportantDates.objects.create(**important_date_data)
-            # Agregar el teléfono al contacto
+            # Add important date to contact
             contact.important_dates.add(important_date)
 
         for related_person_data in related_persons_data:
-            # Crea cada objeto de email relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each related person object related to the contact
             related_person = RelatedPersons.objects.create(**related_person_data)
-            # Agregar el teléfono al contacto
+            # Add related person to contact
             contact.related_persons.add(related_person)
 
         for tag_data in tags_data:
-            # Crea cada objeto de email relacionado con el contacto
-            # Aquí debes adaptar esto según cómo estén definidos tus modelos
+            # Create each tag object related to the contact
             tag = Tags.objects.create(**tag_data)
-            # Agregar el teléfono al contacto
+            # Add tag to contact
             contact.tags.add(tag)
 
         return contact
